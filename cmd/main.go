235d45e74bc477906debc0d6d4e05bb269878967
Aayush goel <mgoel1296@gmail.com>
2025-03-09 19:41:02 +0000

    add get point

--- cmd/main.go
+++ cmd/main.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/goel-aayush/students-api/internal/config"
 	"github.com/goel-aayush/students-api/internal/http/handlers/student"
+	"github.com/goel-aayush/students-api/internal/storage/sqlite"
 )
 
 func main() {
@@ -22,10 +23,19 @@ func main() {
 
 	//custom loger
 	// database setup
+
+	storage, err := sqlite.New(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	slog.Info("storage intialized", slog.String("env", cfg.Env))
 	// setup router
 
 	router := http.NewServeMux()
-	router.HandleFunc("POST /api/students", student.New())
+	router.HandleFunc("POST /api/students", student.New(storage))
+
+	router.HandleFunc("GET /api/students/{id}", student.GetById(storage))
 
 	// setup server
 
@@ -55,7 +65,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	err := server.Shutdown(ctx)
+	err = server.Shutdown(ctx)
 	if err != nil {
 		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
 	}
--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -5,14 +5,17 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
+	"strconv"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/goel-aayush/students-api/internal/storage"
 	"github.com/goel-aayush/students-api/internal/types"
 	"github.com/goel-aayush/students-api/internal/utils/response"
 )
 
-func New() http.HandlerFunc {
+func New(storage storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
 		//w.Write([]byte("welcome to students api"))
@@ -37,7 +40,41 @@ func New() http.HandlerFunc {
 			return
 		}
 
-		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "OK"})
+		lastId, err := storage.CreateStudent(
+			student.Name,
+			student.Email,
+			student.Age,
+		)
+		slog.Info("user created succesfully", slog.String("userId", fmt.Sprint(lastId)))
+		if err != nil {
+			response.WriteJson(w, http.StatusInternalServerError, err)
+			return
+		}
+		response.WriteJson(w, http.StatusCreated, map[string]int64{"id": lastId})
 	}
 
+}
+
+func GetById(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		slog.Info("getting a student ", slog.String("id", id))
+
+		intId, err := strconv.ParseInt(id, 10, 64)
+
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GenralError(err))
+			return
+		}
+
+		student, err := storage.GetStudentById(intId)
+
+		if err != nil {
+			slog.Error("error geting user", slog.String("id", id))
+			response.WriteJson(w, http.StatusInternalServerError, response.GenralError(err))
+			return
+		}
+
+		response.WriteJson(w, http.StatusOK, student)
+	}
 }
